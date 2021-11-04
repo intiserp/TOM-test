9df3910fa9c3d2a29093a988e1bf8dfc91c32313
Intiser Rajoan Parash <irparash@gmail.com>
2021-11-04 04:36:06 -0500

    made order changeable flatlist

--- src/ProjectDocumentation.js
+++ src/ProjectDocumentation.js
@@ -13,95 +13,94 @@ import {
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
-import DraggableFlatList, {
-  RenderItemParams,
-} from "react-native-draggable-flatlist";
-
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
+var randomWords = require("random-words");
 
 const ProjectDocumentation = ({ navigation }) => {
+  let emptyStep = {
+    id: Date.now(),
+    instructions: "",
+    order: 1,
+  };
   const [name, setName] = useState("");
   const [purpose, setPurpose] = useState("");
   const [description, setDescription] = useState("");
   const [history, setHistory] = useState("");
   const [feedback, setFeedback] = useState("");
   const [steps, setSteps] = useState([]);
-  const [billOfMaterials, setBillOfMaterials] = useState([]);
 
-  // adding NEW STEPS!
+  // adding new steps -- this works
   const individualStepAddition = () => {
-    console.log("new step");
-    setSteps(
-      steps.concat(
-        <View style={{ flexDirection: "row" }}>
-          {/* <View
-            style={{
-              backgroundColor: "black",
-              width: 400,
-              height: 40,
-              top: -windowHeight * 0.18,
-              left: 15,
-              fontSize: 20,
-            }}
-          ></View> */}
-
-          {/* 'DRAG' must be pressed to move around the step */}
-          <Text style={{ top: -windowHeight * 0.18, left: 15, fontSize: 17 }}>
-            DRAG
-          </Text>
-          <TextInput
-            placeholder="Step Description"
-            style={styles.stepDescription}
-            // onChangeText={(text) => setName(text)}
-          />
-        </View>
-      )
-    );
-    return;
+    emptyStep.order = steps.length + 1;
+    emptyStep.id = Date.now();
+    emptyStep.instructions = randomWords({ exactly: 2, join: " " });
+    setSteps([...steps, emptyStep]);
   };
 
-  // rendering each step and making them touchable
-  // already tried using useCallback
-  const renderItem = ({ item, index, drag, isActive }) => {
-    // onLongPress={drag}
-    return (
-      <TouchableOpacity onPressIn={drag} style={{ marginTop: 10 }}>
-        {item}
-      </TouchableOpacity>
-    );
+  // removing steps -- this works
+  const individualStepRemoval = (id) => {
+    const currentOrder = steps.find((step) => step.id === id).order;
+    let newSteps = steps.map((step) => {
+      if (step.order > currentOrder) {
+        step.order--;
+      }
+      return step;
+    });
+    newSteps = newSteps.filter((step) => step.id !== id);
+    setSteps(newSteps);
+  };
+
+  // incrementing the order of the steps
+  const incrementOrder = (id) => {
+    const currentOrder = steps.find((step) => step.id === id).order;
+    const newOrder = currentOrder + 1;
+    if (newOrder > steps.length) {
+      return;
+    }
+    const newSteps = steps.map((step) => {
+      if (step.id === id) {
+        return { ...step, order: newOrder };
+      } else if (step.order === newOrder) {
+        return { ...step, order: currentOrder };
+      } else {
+        return step;
+      }
+    });
+    setSteps(newSteps);
+  };
+
+  // decrementing the order of the steps
+  const decrementOrder = (id) => {
+    // switch the order of the steps
+    const currentOrder = steps.find((step) => step.id === id).order;
+    const newOrder = currentOrder - 1;
+    if (newOrder < 1) {
+      return;
+    }
+    const newSteps = steps.map((step) => {
+      if (step.id === id) {
+        return { ...step, order: newOrder };
+      } else if (step.order === newOrder) {
+        return { ...step, order: currentOrder };
+      } else {
+        return step;
+      }
+    });
+    setSteps(newSteps);
   };
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
-    >
-      <View style={{ flexDirection: "column", top: -5 }}>
-        <Image
-          style={styles.greenBackground}
-          source={require("../assets/IndividualStepAddition_Images/green_background.png")}
-        />
-      </View>
-
-      <View style={{ flexDirection: "column" }}>
-        <Image
-          style={styles.logo}
-          source={require("../assets/IndividualStepAddition_Images/tomlogo.png")}
-        />
-      </View>
-
-      <View style={{ flexDirection: "column" }}>
-        <View style={styles.whiteBackground} />
-      </View>
-
-      <View style={styles.projectNameAndEtc}>
+    <SafeAreaView style={{ flex: 1, flexDirection: "column" }}>
+      <View style={{ top: 15 }}>
         <TextInput
           placeholder="Project Name"
           style={styles.projectName}
           onChangeText={(text) => setName(text)}
           value={name}
         />
-
+      </View>
+      <View style={{ top: 25, bottom: 25 }}>
         <TouchableOpacity
           style={styles.purposeInput}
           onPress={() =>
@@ -117,7 +116,7 @@ const ProjectDocumentation = ({ navigation }) => {
             Purpose of Device
           </Text>
         </TouchableOpacity>
-        <View style={{ top: -windowHeight * 0.16, marginBottom: 17 }}>
+        <View style={{ marginBottom: 17 }}>
           <View style={styles.thinBar}></View>
         </View>
 
@@ -137,109 +136,118 @@ const ProjectDocumentation = ({ navigation }) => {
             Physical Description
           </Text>
         </TouchableOpacity>
-        <View style={{ top: -windowHeight * 0.16, marginBottom: 17 }}>
+        <View style={{ marginBottom: 17 }}>
           <View style={styles.thinBar}></View>
         </View>
+      </View>
 
-        <TouchableOpacity
-          style={styles.purposeInput}
-          onPress={() =>
-            navigation.navigate("TextBoxPage", {
-              fieldName: "History of Development",
-              placeholder: "Enter the history of development here",
-              value: history,
-              setValue: setHistory,
-            })
-          }
-        >
-          <Text style={{ fontSize: 17, color: "#C0C0C0" }}>
-            History Of Development
-          </Text>
-        </TouchableOpacity>
-        <View style={{ top: -windowHeight * 0.16, marginBottom: 17 }}>
-          <View style={styles.thinBar}></View>
-        </View>
-
-        <TouchableOpacity
-          style={styles.purposeInput}
-          onPress={() =>
-            navigation.navigate("TextBoxPage", {
-              fieldName: "User Feedback",
-              placeholder: "Enter the user feedback here",
-              value: feedback,
-              setValue: setFeedback,
-            })
-          }
-        >
-          <Text style={{ fontSize: 17, color: "#C0C0C0" }}>User Feedback</Text>
-        </TouchableOpacity>
-        <View style={{ top: -windowHeight * 0.16, marginBottom: 17 }}>
-          <View style={styles.thinBar}></View>
-        </View>
-
-        <TouchableOpacity
-          style={styles.addMaterialsBox}
-          onPress={() => {
-            navigation.navigate("Materials");
-          }}
-        >
-          <Image
-            style={styles.addMaterials}
-            source={require("../assets/projects-11/addmaterials.png")}
-          />
-        </TouchableOpacity>
-
-        <Text
-          style={{
-            left: windowWidth * 0.07,
-            top: -windowHeight * 0.125,
-            fontSize: 18,
-            color: "black",
-            fontWeight: "bold",
-          }}
-        >
-          Instructions
-        </Text>
-
+      <Text
+        style={{
+          left: windowWidth * 0.07,
+          fontSize: 18,
+          color: "black",
+          fontWeight: "bold",
+          marginTop: 40,
+        }}
+      >
+        Instructions
+      </Text>
+      <View
+        style={{
+          position: "absolute",
+          bottom: 80,
+          right: 60,
+          border: "25%",
+          color: "blue",
+          zIndex: 1,
+        }}
+      >
         <TouchableOpacity
           style={styles.plusBox}
           onPress={individualStepAddition}
         >
-          <Image
-            style={styles.plus}
-            source={require("../assets/projects-11/plus.png")}
-          />
-        </TouchableOpacity>
-
-        <View
-          style={{
-            // left: windowWidth * 0.07,
-            top: -windowHeight * 0.14,
-            width: windowWidth,
-            height: windowHeight * 0.3,
-          }}
-        >
-          <View style={{ flex: 1 }}>
-            {/* <FlatList
-                scrollsToTop={true}
-                data={steps}
-                contentContainerStyle={{ marginTop: 170, paddingBottom: 10 }}
-                renderItem={({ item }) => {
-                  return <View style={{ marginTop: 10 }}>{item}</View>;
-                }}
-              /> */}
-            {/* Draggable flat list here  */}
-            <DraggableFlatList
-              scrollsToTop={true}
-              contentContainerStyle={{ marginTop: 170, paddingBottom: 10 }}
-              data={steps}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-              // this is where the steps array is updated....
-              onDragEnd={({ steps }) => setSteps(steps)}
+          <View
+            style={{
+              borderRadius: 50,
+              backgroundColor: "#f15d31",
+              width: 50,
+              height: 50,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              style={styles.plus}
+              source={require("../assets/projects-11/plus.png")}
             />
           </View>
-        </View>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <FlatList
+          scrollsToTop={true}
+          data={steps.sort((a, b) => a.order - b.order)}
+          contentContainerStyle={{
+            marginTop: 5,
+            paddingBottom: 5,
+          }}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            return (
+              // make a nice box for each step
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    marginLeft: 20,
+                    marginRight: 20,
+                    borderWidth: 1,
+                    borderColor: "#C0C0C0",
+                    borderRadius: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 17, color: "black", padding: 10 }}>
+                    Step {item.order}: {item.instructions}
+                  </Text>
+                  <TouchableOpacity
+                    style={{ position: "absolute", right: 10, top: 10 }}
+                    onPress={() => individualStepRemoval(item.id)}
+                  >
+                    <Image
+                      style={styles.plus}
+                      source={require("../assets/projects-11/delete.png")}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ position: "absolute", right: 90, top: 10 }}
+                    onPress={() => decrementOrder(item.id)}
+                  >
+                    <Image
+                      style={styles.plus}
+                      source={require("../assets/projects-11/arrow-up.png")}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ position: "absolute", right: 50, top: 10 }}
+                    onPress={() => incrementOrder(item.id)}
+                  >
+                    <Image
+                      style={styles.plus}
+                      source={require("../assets/projects-11/arrow-down.png")}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            );
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -256,14 +264,12 @@ const styles = StyleSheet.create({
 
   purposeInput: {
     left: windowWidth * 0.07,
-    top: -windowHeight * 0.17,
     fontSize: 17,
     padding: 2.5,
     width: windowWidth * 0.86,
   },
   projectName: {
     left: windowWidth * 0.07,
-    top: -windowHeight * 0.19,
     width: windowWidth * 0.86,
     fontSize: 24,
     color: "black",
@@ -305,10 +311,10 @@ const styles = StyleSheet.create({
   greenBackground: {
     width: windowWidth,
     height: windowHeight * 0.5,
-    left: 0,
+    // left: 0,
     top: -windowHeight * 0.03,
-    position: "relative",
-    flexDirection: "row",
+    // position: "relative",
+    // flexDirection: "row",
   },
 
   logo: {
@@ -330,21 +336,16 @@ const styles = StyleSheet.create({
   },
 
   plusBox: {
-    width: windowWidth * 0.044,
-    height: windowHeight * 0.021,
-    left: windowWidth * 0.896,
-    top: -windowHeight * 0.145,
+    // width: windowWidth * 0.044,
+    // left: windowWidth * 0.896,
+    position: "relative",
+    right: 0,
+    bottom: 0,
   },
 
   plus: {
     width: windowWidth * 0.044,
     height: windowHeight * 0.021,
-  },
-
-  projectNameAndEtc: {
-    left: 0,
-    top: -windowHeight * 0.17,
-    height: windowHeight * 0.27,
   },
 
   stepName: {
@@ -357,17 +358,9 @@ const styles = StyleSheet.create({
   thinBar: {
     backgroundColor: `rgba(112, 112, 112, 0.3)`,
     width: "88%",
-    height: windowWidth * 0.0045,
+    height: 1,
     left: windowWidth * 0.055,
-    // marginTop: 25,
-  },
-
-  thinBar2: {
-    backgroundColor: `rgba(112, 112, 112, 0.3)`,
-    width: "88%",
-    height: windowWidth * 0.0045,
-    left: windowWidth * 0.07,
-    top: -windowHeight * 0.21,
+    top: 5,
   },
 
   uploadImage: {
